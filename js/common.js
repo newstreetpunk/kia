1c6788e17015bfbeb78c27b5d4f4b9f73882b14d
newstreetpunk <newstreetpunk@yandex.ru>
2020-03-12 14:46:49 +0500

    Header mobile

--- js/common.js
+++ js/common.js
@@ -1,16 +1,18 @@
 $(function() {
 
-	var $divs = $('.rand div');
-	var arr = [];
-	$divs.each(function(){
-		arr.push($(this).detach());
+	$('.rand').each(function(){
+		var $divs = $(this).children('div');
+		var arr = [];
+		$divs.each(function(){
+			arr.push($(this).detach());
+		});
+		arr.sort(function(a, b){
+			return Math.random() - 0.5;
+		});
+		for (var index in arr) {
+			$(this).append(arr[index]);
+		}
 	});
-	arr.sort(function(a, b){
-		return Math.random() - 0.5;
-	});
-	for (var index in arr) {
-		$('.rand').append(arr[index]);
-	}
 
 	$('.player').mb_YTPlayer();
 
