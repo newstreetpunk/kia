7bb1bf3981f3bd68e98ab2613ee35a7c57af36c4
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-04-22 19:11:05 +0500

    slick-1.8.1 + banner + libs_gulpfile.js

---
+++ libs_gulpfile.js
@@ -0,0 +1,29 @@
+		scripts_promo: {
+			src: [
+				base.kia + '/libs/jquery/dist/jquery.min.js',
+				base.kia + '/libs/Magnific-Popup-master/jquery.magnific-popup.js',
+				base.kia + '/libs/range/range.js',
+				base.kia + '/libs/animate-css.js',
+				base.kia + '/libs/jquery.mb.YTPlayer.min.js',
+				base.kia + '/libs/lazyload.min.js',
+				base.kia + '/libs/waypoint.js',
+				base.kia + '/js/kia-map.js',
+				base.kia + '/js/common.js', // Custom scripts. Always at the end
+			],
+			dest:       base.kia + '/js',
+			output:     'main-scripts.min.js',
+		},
+		scripts_service: {
+			src: [
+				base.kia + '/libs/jquery/dist/jquery.min.js',
+				base.kia + '/libs/Magnific-Popup-master/jquery.magnific-popup.js',
+				base.kia + '/libs/slick-1.8.1/slick/slick.min.js',
+				base.kia + '/libs/animate-css.js',
+				base.kia + '/libs/lazyload.min.js',
+				base.kia + '/libs/waypoint.js',
+				base.kia + '/js/kia-map.js',
+				base.kia + '/js/service-common.js', // Custom scripts. Always at the end
+			],
+			dest:       base.kia + '/js',
+			output:     'service-scripts.min.js',
+		},
