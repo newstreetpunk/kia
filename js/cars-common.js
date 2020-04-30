8897cfeccd53510105f2a96b5f25f18006968fe5
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-04-30 14:11:40 +0500

    h1

--- js/cars-common.js
+++ js/cars-common.js
@@ -1,5 +1,19 @@
 jQuery(function($) {
 
+	$('.rand').each(function(){
+		var $divs = $(this).children('div');
+		var arr = [];
+		$divs.each(function(){
+			arr.push($(this).detach());
+		});
+		arr.sort(function(a, b){
+			return Math.random() - 0.5;
+		});
+		for (var index in arr) {
+			$(this).append(arr[index]);
+		}
+	});
+
 	//E-mail Ajax Send
 	$("form").submit(function() { //Change
 		var th = $(this);
