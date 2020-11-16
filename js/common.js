3719ac24f8d15e08db3614842d5ce284df5c490a
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-11-16 12:04:41 +0500

    ссылки на youtube

--- js/common.js
+++ js/common.js
@@ -148,6 +148,41 @@ jQuery(function($) {
 		
 	});
 
+	// Video Load & Popup
+	$('.video-popup').magnificPopup({
+		disableOn: 700,
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: true,
+
+		fixedContentPos: true,
+		type: 'iframe',
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">'+
+			'<div class="mfp-close"></div>'+
+			'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+			'</div>',
+			patterns: {
+				youtube: {
+					index: 'youtube.com/', 
+					id: 'v=',
+					src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: '/',
+					src: '//player.vimeo.com/video/%id%?autoplay=1'
+				},
+				gmaps: {
+					index: '//maps.google.',
+					src: '%id%&output=embed'
+				}
+			},
+			srcAction: 'iframe_src', 
+		}
+	});
+	//
+
 	$(function() {
 	    count = -1; // start with -1
 	    initText = "от стоимости автомобиля"; //set your init text here
